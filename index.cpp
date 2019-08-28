a2be1ae5878bb201796aeb01576fd5eaafbd3fad
Quanluis <quanluis@protonmail.com>
2019-08-28 17:03:12 -0400

    created a simple program that recieves the total amount of two numbers and displays the profits

--- index.cpp
+++ index.cpp
@@ -2,7 +2,20 @@
 using namespace std;
 
 int main () {
-    cout << "Hello world";
-    
+
+    double cost, supply, total;
+
+    cout << "Last years purchase cost." << endl;
+
+    cout << "Purchase cost of each baseball" << endl;
+    cin >> cost ;
+
+    cout << "number of baseballs purchased" << endl;
+    cin >> supply ;
+
+    total = supply * cost;
+
+    cout << "The total amount spent was " << total << "$" << endl;
+
     return 0;
 }
